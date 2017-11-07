b129ee0cb1817a82c8c651c0b7e35277357d9a21
Scott Bolinger <designbyscott@gmail.com>
2017-11-07 13:15:13 -0800

    add Content-Security-Policy and X-Frame-Options

--- appcors.php
+++ appcors.php
@@ -16,6 +16,10 @@ function app_cors_header() {
 	
 	if( class_exists( 'AppPresser' ) && AppPresser::is_min_ver( 2 ) ) {
 		header("Access-Control-Allow-Origin: *");
+		
+		// The lines below may need to be uncommented if there are still issues
+		// header("X-Frame-Options: GOFORIT");
+		// header("Content-Security-Policy: frame-src *");
 
 		// If you have issues displaying the checkout page in AppWoo, you may need to uncomment this line.
 		// remove_action( 'template_redirect', 'wc_send_frame_options_header' );
